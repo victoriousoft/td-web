1ead5426697ea376311c87de4aec28490981c53a
Kristián Kunc <kristian.kunc@gmail.com>
2024-11-11 00:33:44 +0100

    update game deployment url

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <div>
-	<iframe bind:this={iframe} on:load={onIframeLoad} title="Game window" src="https://td.kristn.co.uk/" class="h-lvh w-full" />
+	<iframe bind:this={iframe} on:load={onIframeLoad} title="Game window" src="https://tower-defense.kristn.co.uk/" class="h-lvh w-full" />
 	<button class="fixed bottom-4 right-4 z-10" on:click={toggleFullscreen}>
 		<img src="icons/fullscreen.webp" alt="Fullscreen" class="h-8 w-8" />
 	</button>
