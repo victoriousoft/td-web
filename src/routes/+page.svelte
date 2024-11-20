87adbd435a725a24209e3fec4f743e742e2a77e7
Kristián Kunc <kristian.kunc@gmail.com>
2024-11-20 18:40:02 +0100

    remove default homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="m-5 text-center text-4xl font-bold">Tower Defense</h1>
+<p class="text-center text-gray-500">Coming soon...</p>
