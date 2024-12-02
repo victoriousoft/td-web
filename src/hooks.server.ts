1bfbe60dd3b9df62182129cf536fa8feb13087ab
Kristián Kunc <kristian.kunc@gmail.com>
2024-12-02 21:45:54 +0100

    restrict game and rework auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,26 +3,12 @@ import { handle as googleHandle } from "$lib/auth";
 import type { Handle } from "@sveltejs/kit";
 import { prisma } from "$lib/prisma";
 
-async function isAdmin(email: string): Promise<boolean> {
-	return (
-		prisma.Admin.findFirst({
-			where: {
-				email
-			}
-		}) !== null
-	);
-}
-
 const setLocals: Handle = async ({ event, resolve }) => {
 	const authSession = await event.locals.auth();
 
 	if (authSession?.user?.name && authSession?.user?.email && authSession?.user?.image) {
-		event.locals.user = {
-			name: authSession.user.name,
-			email: authSession.user.email,
-			image: authSession.user.image,
-			isAdmin: await isAdmin(authSession.user.email)
-		};
+		// @ts-ignore
+		event.locals.user = authSession.user;
 	}
 
 	return resolve(event);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,38 @@
-import { SvelteKitAuth } from "@auth/sveltekit";
+import { SvelteKitAuth, type DefaultSession } from "@auth/sveltekit";
 import Google from "@auth/sveltekit/providers/google";
+import { prisma } from "./prisma";
+
+declare module "@auth/sveltekit" {
+	interface Session extends DefaultSession {
+		user: {
+			isAdmin: boolean;
+		} & DefaultSession["user"];
+	}
+}
+
 export const { handle, signIn } = SvelteKitAuth({
 	trustHost: true,
-	providers: [Google]
+	providers: [Google],
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user && user.email) {
+				token.isAdmin = await isAdmin(user.email);
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			session.user.isAdmin = (token.isAdmin as boolean) || false;
+			return session;
+		}
+	}
 });
+
+async function isAdmin(email: string): Promise<boolean> {
+	return (
+		(await prisma.Admin.findFirst({
+			where: {
+				email
+			}
+		})) !== null
+	);
+}
