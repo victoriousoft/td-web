9d649a6a6dfa5b80a493aeb2bfa69d8e63ad3829
Kristián Kunc <kristian.kunc@gmail.com>
2025-02-12 22:58:07 +0100

    Colors (#3)
    * configure fonts
    
    * update font sizes
    
    * let's not use css vars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,15 @@
 
 <svelte:head>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=edit" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link href="https://fonts.googleapis.com/css2?family=Gideon+Roman&family=Luxurious+Roman&display=swap" rel="stylesheet" />
+	<style>
+		@font-face {
+			font-family: "Seagram TFB";
+			src: url("/fonts/seagram-tfb.ttf") format("truetype");
+		}
+	</style>
 </svelte:head>
 
 <Error />
