c4b5e307c9d948360873821737efec6550bf1bc9
Kristián Kunc <kristian.kunc@gmail.com>
2025-03-20 09:20:06 +0100

    make model preview look nicer

--- src/lib/components/glb-view.svelte
+++ src/lib/components/glb-view.svelte
@@ -5,19 +5,45 @@
 	export let modelUrl = "";
 </script>
 
-<Canvas>
-	<T.AmbientLight intensity={1.0} />
-	<T.DirectionalLight position={[10, 10, 5]} intensity={1.2} />
-	<T.DirectionalLight position={[-10, -10, -5]} intensity={1.2} />
-	<T.DirectionalLight position={[0, 15, 0]} intensity={0.8} />
-	<T.PointLight position={[5, 5, 5]} intensity={1} distance={20} />
-	<T.SpotLight position={[-5, 10, -5]} intensity={0.8} angle={Math.PI / 4} penumbra={0.2} />
+<Canvas shadows>
+	<T.AmbientLight intensity={0.3} />
+	<T.DirectionalLight 
+		position={[10, 10, 5]} 
+		intensity={0.6} 
+		castShadow 
+		shadow-mapSize-width={2048} 
+		shadow-mapSize-height={2048}
+		shadow-camera-far={50}
+		shadow-camera-near={0.5}
+	/>
+	<T.DirectionalLight position={[-10, 5, -5]} intensity={0.3} />
+	<T.HemisphereLight args={["#ddeeff", "#202020", 0.5]} />
+	<T.SpotLight 
+		position={[5, 15, 5]} 
+		intensity={0.7} 
+		angle={Math.PI / 6} 
+		penumbra={0.7} 
+		castShadow
+		shadow-mapSize-width={1024}
+		shadow-mapSize-height={1024}
+	/>
+	
+	<T.Fog color="#f0f0f0" near={50} far={200} />
+	<T.GridHelper args={[100, 100, "#888888", "#444444"]} position.y={-0.01} />
 
-	<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={80} near={0.1} far={1000}>
-		<OrbitControls autoRotate autoRotateSpeed={1.0} allowPan={false} enableDamping maxDistance={200} minDistance={10} />
+	<T.PerspectiveCamera makeDefault position={[15, 10, 15]} fov={60} near={0.1} far={1000}>
+		<OrbitControls 
+			autoRotate 
+			autoRotateSpeed={0.5} 
+			enableDamping 
+			dampingFactor={0.05}
+			maxPolarAngle={Math.PI / 2} 
+			maxDistance={100} 
+			minDistance={5}
+		/>
 	</T.PerspectiveCamera>
 
 	{#await useGltf(modelUrl) then gltf}
-		<T is={gltf.scene} />
+		<T is={gltf.scene} receiveShadow castShadow />
 	{/await}
 </Canvas>
