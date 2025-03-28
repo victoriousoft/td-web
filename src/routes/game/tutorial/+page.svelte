06c4fb12edc201310998707a804279627aa4b1c2
Kristián Kunc <kristian.kunc@gmail.com>
2025-03-28 13:54:32 +0100

    add tutorial page

---
+++ src/routes/game/tutorial/+page.svelte
@@ -0,0 +1,58 @@
+<h1 class="m-5 text-center text-4xl font-bold">Tutorial</h1>
+
+<div class="m-auto mx-5 max-w-4xl md:mx-auto md:w-3/4">
+	<section class="items-left flex flex-col">
+		<h2 class="my-4 text-left text-2xl">Introduction</h2>
+		<div class="text-lg">
+			<p>
+				A local park is being taken over by various evil creatures. The cats are the only ones who can save the park from the invasion. You are the
+				commander of the cat army and your task is to build towers to defend the park from the enemies and not let any of them reach the end. (level
+				image)
+			</p>
+		</div>
+	</section>
+
+	<section class="items-left flex flex-col">
+		<h2 class="my-4 text-left text-2xl">Building towers</h2>
+		<div class="text-lg">
+			<p>You can chose from four classes of towers:</p>
+			<ol class="ml-8 list-decimal">
+				<li>Archer</li>
+				<li>Hacker</li>
+				<li>Barracks</li>
+				<li>Bomber</li>
+			</ol>
+
+			(tower image)
+
+			<p>
+				Towers are all unique and have their own strengths and weaknesses. Some towers deal physical damage and some magical. Each tower can be
+				upgraded up to level 3. After that you can chose from 2 evolutions which are specialized variants with abilities that are charged
+				periodically. Each new tower and any upgrade costs gold, so you need to plan your strategy carefully.
+			</p>
+
+			<div class="my-5"></div>
+
+			<p>
+				In each level, there are designated spots where you can build your towers, each tower can be built anywhere. If you mess up, don't worry, you
+				can sell your tower for a partial refund at any point during the game.
+			</p>
+		</div>
+	</section>
+
+	<section class="items-left flex flex-col">
+		<h2 class="my-4 text-left text-2xl">Enemies</h2>
+		<p class="text-lg">
+			There are various types of enemies, some walk and some run, some of them also fly. Enemy may be imune to a specific type of damage, so you need
+			to plan your strategy accordingly. Upon killing an enemy, you receive gold as a reward which you can use to build more towers or upgrade
+			existing ones. (enemy image)
+		</p>
+		<div class="my-5"></div>
+		<p>
+			Enemies spawn in waves, each wave is stronger than the previous one.The first wave has to be triggered manually to give you time to prepare your
+			defense. After that, waves spawn automatically although you can call them earlier if you feel confident to receive a small amount of bonus gold.
+			If an Enemy manages to pass through your defense, you will lose a certain amount of lives, if you run out of lives, you lose the game and have
+			to start over.
+		</p>
+	</section>
+</div>
