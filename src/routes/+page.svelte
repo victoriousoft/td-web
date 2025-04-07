db47d62857e71d5eeaae70a58935a58ad0daeefa
Kristián Kunc <kristian.kunc@gmail.com>
2025-04-07 22:05:47 +0200

    1 post per line only

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,22 +11,20 @@
 <img src="logo.png" alt="Logo" class="lg:w-1/8 mx-auto mb-5 mt-10 w-1/2 sm:w-1/4 md:w-1/6" />
 
 <h2 class="m-5 mt-10 text-center text-2xl font-bold">Latest updates</h2>
-<div class="flex flex-wrap justify-center">
+<div class="mx-auto grid max-w-2xl gap-5 p-5">
 	{#each data.latestPosts as posts}
-		<div class="w-full p-5 sm:w-1/2">
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>{posts.title}</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<p>{posts.content}</p>
-				</Card.Content>
-				<Card.Footer>
-					<p class="text-sm text-gray-500">
-						Posted at: {posts.createdAt.toLocaleString()}
-					</p>
-				</Card.Footer>
-			</Card.Root>
-		</div>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>{posts.title}</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p>{posts.content}</p>
+			</Card.Content>
+			<Card.Footer>
+				<p class="text-sm text-gray-500">
+					Posted at: {posts.createdAt.toLocaleString()}
+				</p>
+			</Card.Footer>
+		</Card.Root>
 	{/each}
 </div>
